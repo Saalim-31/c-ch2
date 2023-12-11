65ca37126f0da586ea4f7c43e7992c7096eec2a6
Saalim-31 <134718581+Saalim-31@users.noreply.github.com>
2023-12-11 14:17:27 +0000

    Add files via upload

---
+++ ch2oop9.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+    float cel;
+    cout<<"Enter the temperature in celsius ";
+    cin>>cel;
+    cout<<"Temperature in fahrenheit = "<<(cel*9/5)+32<<endl;
+    return 0;
+}
